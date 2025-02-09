86bdf469aab2121709319c6848597a2b8d803dbf
Maria Eduarda Amaral Santos Bezerra <mariaeduardaamaralsb@gmail.com>
2025-02-09 19:41:50 -0300

    Criado DbContext

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
     }
 }
