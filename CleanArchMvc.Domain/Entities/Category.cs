f544217862bb176b51b189eafa7bb0626343e57b
Maria Eduarda Amaral Santos Bezerra <mariaeduardaamaralsb@gmail.com>
2025-02-08 22:21:43 -0300

    Criado o modelo anemico da camada de domain

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id {get; set;}
+        public string? Name {get; set;}
+
+        public ICollection<Product>? Products {get; set;}
+    }
+}
