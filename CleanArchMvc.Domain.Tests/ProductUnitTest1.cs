920049ae7026e9097d1c90d3df26059278304dba
Maria Eduarda Amaral Santos Bezerra <mariaeduardaamaralsb@gmail.com>
2025-02-09 19:09:58 -0300

    Criado classe productTest

---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact(DisplayName = "Created Product")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1,"Product Name","Product Description",9.99m,
+                99,"product image");
+            action.Should().NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Created Product Invalid")]
+        public void CreateProduct_WithInvalidParameters_ResultObjectInvalidState()
+        {
+            Action action = () => new Product(-1,"Product Name","Product Description",9.99m,
+                99,"product image");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+    }
+}
